d222506506433231f8b288a064259b12c28de297
Merblr <merblr@live.com>
2014-04-07 22:25:12 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Note
+
+```zsh
+git clone https://github.com/37lr/Tr.git
+```
+
+```
+cd Tr
+bundle install
+```
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tr::Application.config.secret_key_base = '60b33d218cb3426fac9c632ce0a8c9cb85a5170ec3f23be80d7d412d2429915a24c6039009af0dd5a74b91cdcf4f1fb790e5c97e0000aeab6f1519202cd38559'
